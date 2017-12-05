3f4777c721ff2a7ecf75a62abe6f9c493df73693
zigext <zigext@gmail.com>
2017-12-05 08:25:55 +0700

    Edit content

--- app/containers/Detail.js
+++ app/containers/Detail.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import { View, ScrollView, Text} from 'react-native';
 
 export default class Detail extends Component {
+    constructor(props) {
+        super(props)
+    }
 
     render () {
         return (
             <View>
-              <Text>Detail</Text>
+              <Text>{this.props.trip.title}</Text>
             </View>
         );
     }
